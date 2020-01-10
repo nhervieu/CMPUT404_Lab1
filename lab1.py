bd97eae148b08c6251a5c249f71867fbd6e4874e
Natalie Hervieux <nataliehervieux@pop-os.localdomain>
2020-01-09 22:57:21 -0700

    completed Q8

--- lab1.py
+++ lab1.py
@@ -6,3 +6,7 @@ print(r.__version__)
 #curl step 5
 var = r.get("http://google.com/")
 print(var)
+
+#get this code from github and print the content
+my_python = r.get("https://raw.githubusercontent.com/nhervieu/CMPUT404_Lab1/master/lab1.py")
+print(my_python.text)
